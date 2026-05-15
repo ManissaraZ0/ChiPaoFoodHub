5c80b18042d9eda714af9d9b482257339c824822
Manissara Saejan <manissara2548@gmail.com>
2026-05-16 01:42:17 +0700

    Add REST API integration and Mediator Screen

---
+++ FoodHubClient/FoodHubManagerApp/ManagerApp.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FoodHubManagerApp.Model;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace FoodHubManagerApp
+{
+    public partial class ManagerApp : Form
+    {
+        public ManagerApp()
+        {
+            InitializeComponent();
+            //promotions = new PromotionsPage(this) { Dock = DockStyle.Fill };
+            //addPromotion = new AddPromotionPage(this) { Dock = DockStyle.Fill };
+            //tickets = new TicketsPage(this) { Dock = DockStyle.Fill };
+            //reviews = new ReviewsPage(this) { Dock = DockStyle.Fill };
+        }
+
+        private void ManagerApp_Load(object sender, EventArgs e)
+        {
+            //this.Controls.Add(promotions);
+        }
+
+        public void ChangeScreen(object sender, int restaurantId, int managerId, int action)
+        {
+            //if (sender is PromotionsPage)
+            //{
+            //    if (action == 0)
+            //    {
+            //        this.Controls.Clear();
+            //        this.Controls.Add(promotions);
+            //    }
+            //    else if (action == 1)
+            //    {
+            //        this.Controls.Clear();
+            //        addPromotion.RestaurantId = restaurantId;
+            //        addPromotion.ManagerId = managerId;
+            //        this.Controls.Add(addPromotion);
+            //    }
+            //    else if (action == 2)
+            //    {
+            //        this.Controls.Clear();
+            //        tickets.RestaurantId = restaurantId;
+            //        tickets.ManagerId = managerId;
+            //        this.Controls.Add(tickets);
+            //    }
+            //    else if (action == 3)
+            //    {
+            //        this.Controls.Clear();
+            //        reviews.RestaurantId = restaurantId;
+            //        reviews.ManagerId = managerId;
+            //        this.Controls.Add(reviews);
+            //    }
+            //}
+            //if (sender is AddPromotionPage)
+            //{
+            //    if (action == 0)
+            //    {
+            //        this.Controls.Clear();
+            //        this.Controls.Add(addPromotion);
+            //    }
+            //    else if (action == 1)
+            //    {
+            //        this.Controls.Clear();
+            //        promotions.RestaurantId = restaurantId;
+            //        promotions.ManagerId = managerId;
+            //        this.Controls.Add(promotions);
+            //    }
+            //    else if (action == 2)
+            //    {
+            //        this.Controls.Clear();
+            //        tickets.RestaurantId = restaurantId;
+            //        tickets.ManagerId = managerId;
+            //        this.Controls.Add(tickets);
+            //    }
+            //    else if (action == 3)
+            //    {
+            //        this.Controls.Clear();
+            //        reviews.RestaurantId = restaurantId;
+            //        reviews.ManagerId = managerId;
+            //        this.Controls.Add(reviews);
+            //    }
+            //}
+            //if (sender is TicketsPage)
+            //{
+            //    if (action == 0)
+            //    {
+            //        this.Controls.Clear();
+            //        this.Controls.Add(tickets);
+            //    }
+            //    else if (action == 1)
+            //    {
+            //        this.Controls.Clear();
+            //        addPromotion.RestaurantId = restaurantId;
+            //        addPromotion.ManagerId = managerId;
+            //        this.Controls.Add(addPromotion);
+            //    }
+            //    else if (action == 2)
+            //    {
+            //        this.Controls.Clear();
+            //        promotions.RestaurantId = restaurantId;
+            //        promotions.ManagerId = managerId;
+            //        this.Controls.Add(promotions);
+            //    }
+            //    else if (action == 3)
+            //    {
+            //        this.Controls.Clear();
+            //        reviews.RestaurantId = restaurantId;
+            //        reviews.ManagerId = managerId;
+            //        this.Controls.Add(reviews);
+            //    }
+            //}
+            //if (sender is ReviewsPage)
+            //{
+            //    if (action == 0)
+            //    {
+            //        this.Controls.Clear();
+            //        this.Controls.Add(reviews);
+            //    }
+            //    else if (action == 1)
+            //    {
+            //        this.Controls.Clear();
+            //        addPromotion.RestaurantId = restaurantId;
+            //        addPromotion.ManagerId = managerId;
+            //        this.Controls.Add(addPromotion);
+            //    }
+            //    else if (action == 2)
+            //    {
+            //        this.Controls.Clear();
+            //        tickets.RestaurantId = restaurantId;
+            //        tickets.ManagerId = managerId;
+            //        this.Controls.Add(tickets);
+            //    }
+            //    else if (action == 3)
+            //    {
+            //        this.Controls.Clear();
+            //        promotions.RestaurantId = restaurantId;
+            //        promotions.ManagerId = managerId;
+            //        this.Controls.Add(promotions);
+            //    }
+            //}
+        }
+    }
+}
--- FoodHubClient/FoodHubManagerApp/Service.cs
+++ FoodHubClient/FoodHubManagerApp/Service.cs
@@ -1,11 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FoodHubManagerApp.Model;
+using RestApiUtil;
 
-namespace FoodHubManagerApp;
-
-public class Service
+namespace FoodHubCustomerApp
 {
+    public static class Service
+    {
+        // ==========================================
+        // ฝั่ง Customer (เรียกใช้ CustomerController)
+        // ==========================================
+
+        public static List<Promotion> BrowsePromotions(int? restaurantId = null)
+        {
+            string query = restaurantId.HasValue ? $"?restaurantId={restaurantId.Value}" : "";
+            return RestUtil.Get<List<Promotion>>(MyConfig.BaseUri, $"Customer/v1/promotions{query}");
+        }
+
+        public static PromotionTicket BuyPromotionTicket(int promotionId, int userId)
+        {
+            // เนื่องจากไม่มี Body ที่ต้องส่งไป ใช้ PostWithResult แบบส่ง null ไปแทน
+            return RestUtil.PostWithResult<object, PromotionTicket>(
+                MyConfig.BaseUri, $"Customer/v1/promotions/{promotionId}/tickets?userId={userId}", null);
+        }
+
+        public static List<Review> GetReviews(int restaurantId)
+        {
+            return RestUtil.Get<List<Review>>(MyConfig.BaseUri, $"Customer/v1/restaurants/{restaurantId}/reviews");
+        }
+
+        public static Review SubmitReview(int restaurantId, SubmitReviewReq req)
+        {
+            return RestUtil.PostWithResult<SubmitReviewReq, Review>(
+                MyConfig.BaseUri, $"Customer/v1/restaurants/{restaurantId}/reviews", req);
+        }
+
+        public static List<RestaurantRecommendationRsp> GetRecommendedRestaurants()
+        {
+            return RestUtil.Get<List<RestaurantRecommendationRsp>>(MyConfig.BaseUri, "Customer/v1/restaurants/recommendations");
+        }
+
+        public static CustomerProfileRsp GetCustomerProfile(int userId)
+        {
+            return RestUtil.Get<CustomerProfileRsp>(MyConfig.BaseUri, $"Customer/v1/profile?userId={userId}");
+        }
+
+        public static RestaurantDetailRsp GetRestaurantDetail(int restaurantId)
+        {
+            return RestUtil.Get<RestaurantDetailRsp>(MyConfig.BaseUri, $"Customer/v1/restaurants/{restaurantId}/details");
+        }
+
+
+        // ==========================================
+        // ฝั่ง Manager (เรียกใช้ ManagerController)
+        // ==========================================
+
+        public static Promotion AddPromotion(int restaurantId, int managerId, Promotion newPromotion)
+        {
+            return RestUtil.PostWithResult<Promotion, Promotion>(
+                MyConfig.BaseUri, $"Manager/v1/restaurants/{restaurantId}/promotions?managerId={managerId}", newPromotion);
+        }
+
+        public static List<PromotionTicket> GetRestaurantTickets(int restaurantId, int managerId)
+        {
+            return RestUtil.Get<List<PromotionTicket>>(
+                MyConfig.BaseUri, $"Manager/v1/restaurants/{restaurantId}/tickets?managerId={managerId}");
+        }
+
+        public static PromotionTicket ValidateTicket(int ticketId, int managerId)
+        {
+            // ไม่มี Body ที่ต้องส่ง ใช้ null
+            return RestUtil.PostWithResult<object, PromotionTicket>(
+                MyConfig.BaseUri, $"Manager/v1/tickets/{ticketId}:validate?managerId={managerId}", null);
+        }
+
+        public static List<ManagerPromotionSummaryRsp> GetPromotionSummaries(int restaurantId)
+        {
+            return RestUtil.Get<List<ManagerPromotionSummaryRsp>>(
+                MyConfig.BaseUri, $"Manager/v1/restaurants/{restaurantId}/promotions/summary");
+        }
+
+        public static List<ManagerTicketDetailRsp> GetTicketDetails(int restaurantId)
+        {
+            return RestUtil.Get<List<ManagerTicketDetailRsp>>(
+                MyConfig.BaseUri, $"Manager/v1/restaurants/{restaurantId}/tickets-details");
+        }
+
+        public static List<ManagerReviewDetailRsp> GetReviewDetails(int restaurantId)
+        {
+            return RestUtil.Get<List<ManagerReviewDetailRsp>>(
+                MyConfig.BaseUri, $"Manager/v1/restaurants/{restaurantId}/reviews-details");
+        }
+    }
 }
